70467c37af77d76d6fce694ef35f0cebd64d4bc1
Neeraj <neeraj@Neerajs-MacBook-Pro.local>
2014-10-28 17:03:10 +0530

    Design changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017053030) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20141015135245) do
 
   create_table "admins", force: true do |t|
     t.string   "username",           null: false
@@ -24,13 +21,6 @@ ActiveRecord::Schema.define(version: 20141017053030) do
   end
 
   add_index "admins", ["username"], name: "index_admins_on_username", using: :btree
-
-  create_table "api_keys", force: true do |t|
-    t.string   "token"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "events", force: true do |t|
     t.string   "name"
