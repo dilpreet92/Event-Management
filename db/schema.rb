1bf838ddb0d5e5cf007031799ee33bdcd2a39ca1
Neeraj <neeraj@Neerajs-MacBook-Pro.local>
2014-10-28 17:03:10 +0530

    Design changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030140352) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20141015135245) do
 
   create_table "admins", force: true do |t|
     t.string   "username",           null: false
