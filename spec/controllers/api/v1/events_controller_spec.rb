0e515522667574c5da7590543f9b1bde59f5cc6f
dilpreet92 <dilpreet@vinsol.com>
2014-10-30 10:27:37 +0530

    Rspec temporary commit

--- spec/controllers/api/v1/events_controller_spec.rb
+++ spec/controllers/api/v1/events_controller_spec.rb
@@ -10,15 +10,31 @@ describe Api::V1::EventsController do
   end
 
   context '#index' do
-    it 'should render the index '
+    before do
+      @events = double(:events)
+      Event.stub_chain(:enabled, :live_and_upcoming).and_return(@events)
+      get :index, :format => 'json'
+    end
+    it 'should render the index template' do
+      expect(response).to render_template :index
+    end
+    it 'should assign events' do
+      expect(assigns[:events]).to eql @events
+    end
   end
 
   context '#attendees' do
+    it 'should render the attendees template'
+    it 'should assign users'
   end
 
   context '#mine_events' do
+    it 'should render mine_events template'
+    it 'should assign events'
   end
 
   context '#rsvps' do
+    it 'should render rsvps'
+    it 'should assign events'
   end
 end
