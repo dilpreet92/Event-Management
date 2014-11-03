86d84808b4c1cb1b9c1079fd9b453293652645d0
dilpreet92 <dilpreet@vinsol.com>
2014-11-03 19:14:04 +0530

    Change in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030140352) do
+
+ActiveRecord::Schema.define(version: 20141015135245) do
+
+  enable_extension "plpgsql"
 
   create_table "admins", force: true do |t|
     t.string   "username",           null: false
