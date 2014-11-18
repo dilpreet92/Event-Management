188efb31f0afe686faf83ecdc2251bec7e337ee7
dilpreet92 <dilpreet@vinsol.com>
2014-11-18 16:41:04 +0530

    Deploying to production

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,14 +4,19 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
+role :app, %w{deploy@example.com}
+role :web, %w{deploy@example.com}
+role :db,  %w{deploy@example.com}
+
+
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
-set :branch, :production
 
-server '106.187.52.252', user: 'deploy', roles: %w{vinsol}
+server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
 
 # Custom SSH Options
 # ==================
