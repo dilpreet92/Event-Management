25760414f2712b1542cfd8e55ac187e0ca67756d
dilpreet92 <dilpreet@vinsol.com>
2014-11-21 21:21:10 +0530

    Fixing add more speakers for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  @admin = Admin.create('dp', 'Dsingh123#')
+  @admin.save
