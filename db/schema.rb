0e974ccbd305772ee637cdf76cd1b833db3592e5
dilpreet92 <dilpreet@vinsol.com>
2015-01-12 18:52:21 +0530

    temporary fix

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 20141127061548) do
 
   add_index "admins", ["username"], name: "index_admins_on_username", using: :btree
 
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
   create_table "events", force: true do |t|
     t.string   "name"
     t.datetime "start_date"
